16fcbb585f33eaf0020a8f49a0dbec9b8cf871c1
AkashRavindran2005 <akashravindran2500@gmail.com>
2026-02-28 06:43:15 +0530

    update

--- red-team-site/challenge/login_legacy.php
+++ red-team-site/challenge/login_legacy.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $query = "SELECT * FROM users WHERE (username = '$username' OR '$username' != '') AND (password = '$password' OR '$username' = '$username')";
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 
     $result = mysqli_query($conn, $query);
 
